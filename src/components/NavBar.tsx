0bb0d3d4fe235dc60a757774e94c4bfa125448a7
Your Name <you@example.com>
2023-06-07 13:04:31 -0400

    Create a custom hook to fetch the games

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,12 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
+import { HStack, Image } from '@chakra-ui/react'
 import logo from '../assets/Logo/logo.webp';
+import ColorModeSwitch from './ColorModeSwitch';
+
 const NavBar = () => {
   return (
- <HStack>
-<Image src={logo} boxSize={100}/>
-<Text>MervBar</Text>
+ <HStack justifyContent='space-between' padding = '10px'>
+<Image src={logo} boxSize='60px'/>
+<ColorModeSwitch/>
  </HStack>
   )
 }
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,16 +1,30 @@
-Your Current Plan
-Free
-Number of requests left for the period
-20000
-Number of requests will renew on
-2023-07-06
-Full name
-mike mervin
-Contact email
-mikemervin@me.com
-Site/App url
-Describe in a few words how you would like to use RAWG’s API
-mosh course
-API Key
-cb12350bf44c4002940f11cce1cb838c
-Refresh Key
+import axios from "axios";
+
+export default axios.create({
+    baseURL:'https://api.rawg.io/api',
+    params: {
+        key: 'cb12350bf44c4002940f11cce1cb838c'
+    }
+})
+
+
+
+
+
+
+// Your Current Plan
+// Free
+// Number of requests left for the period
+// 20000
+// Number of requests will renew on
+// 2023-07-06
+// Full name
+// mike mervin
+// Contact email
+// mikemervin@me.com
+// Site/App url
+// Describe in a few words how you would like to use RAWG’s API
+// mosh course
+// API Key
+// cb12350bf44c4002940f11cce1cb838c
+// Refresh Key
